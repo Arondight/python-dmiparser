f07cddfe99180686948dca6bddce2338ba8c4db6
Qin Fandong <shell_way@foxmail.com>
2022-07-14 17:53:20 +0800

    Update docs and format code

--- tests/test_dmidecode_10.py
+++ tests/test_dmidecode_10.py
@@ -4,16 +4,17 @@ from dmiparser import DmiParser
 
 RDIR = Path(Path(__file__).resolve()).parents[0]
 
+
 def test_dmidecode_10():
     testnum = 0
     on_board_device = 0
 
-    with open(RDIR / 'dmidecode_10.txt', 'rt') as f:
+    with open(RDIR / "dmidecode_10.txt", "rt") as f:
         text = f.read()
         data = json.loads(str(DmiParser(text)))
 
     for d in data:
-        '''
+        """
         Handle 0x005F, DMI type 10, 20 bytes
         On Board Device 1 Information
                 Type: Video
@@ -23,12 +24,11 @@ def test_dmidecode_10():
                 Type: Ethernet
                 Status: Enabled
                 Description: Intel I350
-        '''
-        if '0x005F' == d['handle']['id']:
+        """
+        if "0x005F" == d["handle"]["id"]:
             on_board_device += 1
 
-    assert(8 == on_board_device)
+    assert 8 == on_board_device
     testnum += 1
 
-    assert(1 == testnum)
-
+    assert 1 == testnum
--- tests/test_dmidecode_9.py
+++ tests/test_dmidecode_9.py
@@ -4,20 +4,20 @@ from dmiparser import DmiParser
 
 RDIR = Path(Path(__file__).resolve()).parents[0]
 
+
 def test_dmidecode_9():
     testnum = 0
 
-    with open(RDIR / 'dmidecode_9.txt', 'rt') as f:
+    with open(RDIR / "dmidecode_9.txt", "rt") as f:
         text = f.read()
         data = json.loads(str(DmiParser(text)))
 
     for d in data:
-        '''
+        """
         Bus Address: 0000:00:03.2
-        '''
-        if '0x0007' == d['handle']['id'] and 'System Slot Information' == d['name']:
-            assert('0000:00:03.2' == d['props']['Bus Address']['values'][0])
+        """
+        if "0x0007" == d["handle"]["id"] and "System Slot Information" == d["name"]:
+            assert "0000:00:03.2" == d["props"]["Bus Address"]["values"][0]
             testnum += 1
 
-    assert(1 == testnum)
-
+    assert 1 == testnum
